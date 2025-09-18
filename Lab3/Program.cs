a1654fbb37449042e82738ce43f11827531db50d
Captain2025 <uragasan9@gmail.com>
2025-09-18 12:00:27 +0300

    ервый коммит: добавил проект

---
+++ Lab3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Program
+{
+    static void First()
+    {
+        Second(); // можно вызвать функцию, определенную ниже
+        Console.WriteLine("First done");
+    }
+
+    static void Second()
+    {
+        Console.WriteLine("Second done");
+    }
+
+    static void Main()
+    {
+        First();
+    }
+}
